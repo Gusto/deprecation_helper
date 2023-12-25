d42d2269256cecec641a345b9d7250433c206449
Xenor Chang <tubaxenor@gmail.com>
2023-12-25 15:39:30 +0800

    Loosen logger's typing to allow different loggers

--- lib/deprecation_helper/strategies/log_error.rb
+++ lib/deprecation_helper/strategies/log_error.rb
@@ -7,9 +7,9 @@ module DeprecationHelper
 
       extend T::Sig
 
-      sig { params(logger: T.nilable(Logger)).void }
+      sig { params(logger: T.untyped).void }
       def initialize(logger: nil)
-        @logger = T.let(logger || Logger.new(STDOUT), Logger)
+        @logger = T.let(logger || Logger.new(STDOUT), T.untyped)
       end
 
       sig { override.params(message: String, backtrace: T::Array[String]).void }
